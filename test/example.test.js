616f05549b9d02c32575ae605eb0e3333b94da4e
Jacob Harrington <jharrington4@gmail.com>
2019-10-31 11:32:28 -0700

    dummy test created for travis

--- test/example.test.js
+++ test/example.test.js
@@ -6,11 +6,14 @@ const test = QUnit.test;
 test('time to test a function', function(assert) {
     //Arrange
     // Set up your parameters and expectations
+    let a = 1;
+    let b = 2;
 
+    const expected = 3;
     //Act 
     // Call the function you're testing and set the result to a const
-
+    const result = a + b;
     //Assert
     // Make assertions about what is expected valid result
-    assert.equal(true, false);
+    assert.equal(expected, result);
 });
